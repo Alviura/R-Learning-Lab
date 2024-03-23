f6592448a9daf5890b90da1eb8d7d0af8493421b
Alvin <alviura@outlook.com>
2024-03-23 21:22:08 +0300

    Conditional statements

---
+++ Conditionals.R
@@ -0,0 +1,16 @@
+# Conditional Statements -> They are used to execute different blocks of code based on certain conditions.
+# The basic conditional statements are 
+# 1) if
+# 2) if...else
+# 3) if...elseif...else
+
+x <- 7
+if(x > 10){
+  print("x is greater than 10")
+}else if(x > 5){
+  print("x is greater than 5")
+}else{
+  print("x is less than or equal to 5")
+}
+
+# the above code summaries all the three condtions
